90039692578a1111650609d94c740291c760911e
Gaël PORT <gael.port@rbs.fr>
2011-02-22 16:34:00 +0100

    [REFACTOR] Move addTreeAttributes and addFormProperties from final document class to document service.

--- persistentdocument/ws.class.php
+++ persistentdocument/ws.class.php
@@ -5,20 +5,4 @@
  */
 class webservices_persistentdocument_ws extends webservices_persistentdocument_wsbase 
 {
-	/**
-	 * @param string $moduleName
-	 * @param string $treeType
-	 * @param array<string, string> $nodeAttributes
-	 */
-//	protected function addTreeAttributes($moduleName, $treeType, &$nodeAttributes)
-//	{
-//	}
-	
-	/**
-	 * @param string $actionType
-	 * @param array $formProperties
-	 */
-//	public function addFormProperties($propertiesNames, &$formProperties)
-//	{	
-//	}
 }
