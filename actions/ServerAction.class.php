1e0b6d53ef6685f3b82618e721ac7c92738d3c6c
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-04-08 11:37:28 +0200

    [NEW] Add exception log

--- actions/ServerAction.class.php
+++ actions/ServerAction.class.php
@@ -89,6 +89,7 @@ class webservices_ServerAction extends f_action_BaseAction
 		catch (Exception $e)
 		{
 			Framework::exception($e);
+			webservices_ModuleService::getInstance()->log("SOAP RESPONSE EXCEPTION $serviceName: " . $e->getCode() .','. $e->getMessage());
 			$server->fault($e->getCode(), $e->getMessage());
 		}
 		return null;
